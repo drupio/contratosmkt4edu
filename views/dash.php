cce3074986d554e11ae41c210afa58af16f359b5
Drupio <drupio@gmail.com>
2022-01-20 16:17:12 -0300

    graficos

--- views/dash.php
+++ views/dash.php
@@ -62,7 +62,7 @@ include '../includes/menu.php';
                                         role: 'annotation'
                                     }],
                                     <?php
-                                    $sqlc = "SELECT *, count(status_cliente) AS total FROM clientes AS c JOIN status_clientes AS s ON c.status_cliente = s.id_status GROUP BY status_cliente";
+                                    $sqlc = "SELECT *, count(status_cliente) AS total FROM clientes AS c JOIN status_clientes AS s ON c.status_cliente = s.id_status GROUP BY c.status_cliente, s.id_status";
                                     $resc = mysqli_query($conexao, $sqlc);
                                     while ($cliente = mysqli_fetch_array($resc)) :
                                     ?>['<?php echo $cliente['nome_status'] ?>', <?php echo $cliente['total'] ?>, <?php echo $cliente['total'] ?>],
@@ -132,52 +132,8 @@ include '../includes/menu.php';
                     </div>
                 </div>
             <?php endwhile; ?>
-
-            <div class="col-md-6">
-                <div id="chart_div"></div>
-                <script type="text/javascript">
-                    // Load the Visualization API and the corechart package.
-                    google.charts.load('current', {
-                        'packages': ['corechart']
-                    });
-
-                    // Set a callback to run when the Google Visualization API is loaded.
-                    google.charts.setOnLoadCallback(drawChart);
-
-                    // Callback that creates and populates a data table,
-                    // instantiates the pie chart, passes in the data and
-                    // draws it.
-                    function drawChart() {
-
-                        // Create the data table.
-                        var data = new google.visualization.DataTable();
-                        data.addColumn('string', 'Topping');
-                        data.addColumn('number', 'Slices');
-                        data.addRows([
-                            ['Mushrooms', 3],
-                            ['Onions', 1],
-                            ['Olives', 1],
-                            ['Zucchini', 1],
-                            ['Pepperoni', 2]
-                        ]);
-
-                        // Set chart options
-                        var options = {
-                            'title': 'How Much Pizza I Ate Last Night',
-                            'width': 400,
-                            'height': 300
-                        };
-
-                        // Instantiate and draw our chart, passing in some options.
-                        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-                        chart.draw(data, options);
-                    }
-                </script>
-            </div>
         </div>
     </div>
-</div>
-</div>
 </div>
 <?php
 include '../includes/modal.php';
